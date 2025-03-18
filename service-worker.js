782b0cf88e600b009b32368eb49653954f60dc7e
ManoharGolleru <ManoharGolleru@users.noreply.github.com>
2025-03-18 22:13:46 +0000

    deploy: f33b947db0141dba295fee0e04fed810b6be4df2

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 var GHPATH = "/OS-DPI";
 var APP_PREFIX = "osdpi_";
-var VERSION = "2025-2-18-22-6-12";
+var VERSION = "2025-2-18-22-13-38";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
